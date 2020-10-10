890990c12c4aca1c8cea9dc80340411383a89f9e
chenlw <chenlw@bytter.com>
2020-10-10 18:01:35 +0800

    update

--- src/test/java/com/haiyang/flowablespringboot/DynamicActivitiProcessTest.java
+++ src/test/java/com/haiyang/flowablespringboot/DynamicActivitiProcessTest.java
@@ -1,19 +1,20 @@
 package com.haiyang.flowablespringboot;
 
-import org.apache.commons.io.FileUtils;
-import org.flowable.bpmn.model.*;
 import org.flowable.bpmn.model.Process;
+import org.flowable.bpmn.model.*;
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
-import org.flowable.engine.ProcessEngines;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.task.api.Task;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.util.StreamUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.List;
 
 /**
@@ -22,12 +23,10 @@ import java.util.List;
  */
 public class DynamicActivitiProcessTest {
 
-
-    @Test
-    public void testDynamicDeploy() throws Exception {
+    protected ProcessEngine initProcessEngine() {
         ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
                 .createStandaloneProcessEngineConfiguration();
-        //Jdbc设置
+        // Jdbc设置
         String jdbcDriver = "com.mysql.cj.jdbc.Driver";
         processEngineConfiguration.setJdbcDriver(jdbcDriver);
         String jdbcUrl = "jdbc:mysql://127.0.0.1:3307/flowable_test?serverTimezone=UTC&nullCatalogMeansCurrent=true";
@@ -36,22 +35,77 @@ public class DynamicActivitiProcessTest {
         processEngineConfiguration.setJdbcUsername(jdbcUsername);
         String jdbcPassword = "123456";
         processEngineConfiguration.setJdbcPassword(jdbcPassword);
-        //        public static final String DB_SCHEMA_UPDATE_FALSE = "false";//不自动创建新表
+        // public static final String DB_SCHEMA_UPDATE_FALSE = "false";//不自动创建新表
         /**
          * Creates the schema when the process engine is being created and drops
          * the schema when the process engine is being closed.
          */
-        //        public static final String DB_SCHEMA_UPDATE_CREATE_DROP = "create-drop";//每次运行创建新表
+        // public static final String DB_SCHEMA_UPDATE_CREATE_DROP = "create-drop";//每次运行创建新表
         /**
          * Upon building of the process engine, a check is performed and an
          * update of the schema is performed if it is necessary.
          */
-        //        public static final String DB_SCHEMA_UPDATE_TRUE = "true";设置自动对表结构进行改进和升级
-        //设置是否自动更新
+        // public static final String DB_SCHEMA_UPDATE_TRUE = "true";设置自动对表结构进行改进和升级
+        // 设置是否自动更新
         processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-        //获取引擎对象
+        // 获取引擎对象
         ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
         processEngine.close();
+        return processEngine;
+    }
+
+    @Test
+    public void testGenerateModel() throws Exception {
+        ProcessEngine processEngine = initProcessEngine();
+
+        // 1. Build up the model from scratch
+        BpmnModel model = new BpmnModel();
+        Process process = new Process();
+        model.addProcess(process);
+        process.setId("my-process");
+
+        process.addFlowElement(createStartEvent());
+        process.addFlowElement(createUserTask("task1", "First task", "fred"));
+        process.addFlowElement(createUserTask("task2", "Second task", "john"));
+        process.addFlowElement(createEndEvent());
+
+        process.addFlowElement(createSequenceFlow("start", "task1"));
+        process.addFlowElement(createSequenceFlow("task1", "task2"));
+        process.addFlowElement(createSequenceFlow("task2", "end"));
+
+    }
+
+    /**
+     * 获取流程模型的XML表示
+     *
+     * @throws Exception 异常
+     */
+    @Test
+    public void testGetModelXml() throws Exception {
+        ProcessEngine processEngine = initProcessEngine();
+        InputStream processBpmn = processEngine.getRepositoryService().getResourceAsStream("5001", "dynamic-model.bpmn");
+        String xml = StreamUtils.copyToString(processBpmn, Charset.forName("UTF-8"));
+        StreamUtils.copy(processBpmn, new FileOutputStream(new File("diagram.png")));
+        Assert.assertNotNull(xml);
+    }
+
+    /**
+     * 获取流程模型的XML表示
+     *
+     * @throws Exception 异常
+     */
+    @Test
+    public void testGetModelImg() throws Exception {
+        ProcessEngine processEngine = initProcessEngine();
+        InputStream processDiagram = processEngine.getRepositoryService().getProcessDiagram("Expense:1:4");
+        Assert.assertNotNull(processDiagram);
+        StreamUtils.copy(processDiagram, new FileOutputStream(new File("diagram.png")));
+        // FileUtils.copyInputStreamToFile(processDiagram, new File("diagram.png"));
+    }
+
+    @Test
+    public void testDynamicDeploy() throws Exception {
+        ProcessEngine processEngine = initProcessEngine();
 
         // 1. Build up the model from scratch
         BpmnModel model = new BpmnModel();
@@ -75,11 +129,11 @@ public class DynamicActivitiProcessTest {
         Deployment deployment = processEngine.getRepositoryService().createDeployment()
                 .addBpmnModel("dynamic-model.bpmn", model).name("Dynamic process deployment").deploy();
 
-        // 4. Start a process instance
+        // 4. Start a process instance  启动一个流程处理实例
         ProcessInstance processInstance = processEngine.getRuntimeService()
                 .startProcessInstanceByKey("my-process");
 
-        // 5. Check if task is available
+        // 5. Check if task is available  检查任务是否可用
         List<Task> tasks = processEngine.getTaskService().createTaskQuery()
                 .processInstanceId(processInstance.getId()).list();
 
@@ -88,12 +142,12 @@ public class DynamicActivitiProcessTest {
         Assert.assertEquals("fred", tasks.get(0).getAssignee());
 
         // 6. Save process diagram to a file
-        InputStream processDiagram = processEngine.getRepositoryService().getProcessDiagram(processInstance.getProcessDefinitionId());
-        FileUtils.copyInputStreamToFile(processDiagram, new File("target/diagram.png"));
+        // InputStream processDiagram = processEngine.getRepositoryService().getProcessDiagram(processInstance.getProcessDefinitionId());
+        // FileUtils.copyInputStreamToFile(processDiagram, new File("target/diagram.png"));
 
         // 7. Save resulting BPMN xml to a file
-        InputStream processBpmn = processEngine.getRepositoryService().getResourceAsStream(deployment.getId(), "dynamic-model.bpmn");
-        FileUtils.copyInputStreamToFile(processBpmn, new File("target/process.bpmn20.xml"));
+        // InputStream processBpmn = processEngine.getRepositoryService().getResourceAsStream(deployment.getId(), "dynamic-model.bpmn");
+        // FileUtils.copyInputStreamToFile(processBpmn, new File("target/process.bpmn20.xml"));
     }
 
     protected UserTask createUserTask(String id, String name, String assignee) {
